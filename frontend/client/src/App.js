22f40fb829d45972be748419a220d482c16350fd
Souvik Sen <souvik22sen@gmail.com>
2022-01-04 19:28:05 +0530

    Frontend Client updated

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,7 +1,30 @@
-import "antd/dist/antd.css";
+import Header from "../src/components/header";
+import Body from "../src/components/body";
+import Footer from "../src/components/footer";
+import Item from "../src/components/Item";
+import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <div className="container">
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </div>
+    </>
+  );
 }
 
 export default App;
